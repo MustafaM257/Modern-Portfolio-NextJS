a1df82592538ae0afce9d0e25de0b9361d0532c4
Mustafa Muhammad <mustafa.m257@proton.me>
2024-04-05 02:19:34 -0400

    Added particles, next to finalize Home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
 import Header from "./components/shared/Header";
-export default function Home() {
+import Home from "./components/Home";
+export default function Page() {
   return (
     <main className="bg-neutral-900 h-[600vh]">
       <Header />
+      <Home />
     </main>
   );
 }

69ed492665d19543d906bbba59adc5df4097d1cc
Mustafa Muhammad <mustafa.m257@proton.me>
2024-04-07 01:20:34 -0400

    Finalized card UI, next to finalize functionality and start with aother section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,23 +2,24 @@ import { cn } from "@/utils/cn";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/projects-bento-grid";
 import {
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
+  IconCalendarEvent, // Events Management System
+  IconBooks, // Learning Management System
+  IconUserSearch, // Github Search App
+  IconDesk, // Job Portal
+  IconUserScreen, // Modern Portfolio
+  IconUnlink, // URL Shortener
 } from "@tabler/icons-react";
 import Container from "./shared/Container";
 
 export function Projects() {
   return (
     <Container id="projects" title="Projects">
-      <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+      <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[12rem]">
         {items.map((item, i) => (
           <BentoGridItem
             key={i}
             title={item.title}
             description={item.description}
-            header={item.header}
             className={item.className}
             icon={item.icon}
           />
@@ -27,63 +28,54 @@ export function Projects() {
     </Container>
   );
 }
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-);
+
 const items = [
   {
     title: "Events Management System",
     href: "https://github.com/MustafaM257/Events-Application-NextJS",
     description:
       "Next.js, TypeScript, Tailwind CSS, Stripe, REST API, Serverless Functions, MongoDB, Mongoose, Clerk Auth, Webhooks.",
-    header: <Skeleton />,
     className: "md:col-span-2",
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    icon: <IconCalendarEvent className="h-8 w-8 text-neutral-500" />,
+  },
+  {
+    title: "Job Portal APIS",
+    href: "https://github.com/MustafaM257/Jobs-API",
+    description: "Node.js, Express, MongoDB, Mongoose, REST API, JWT, Bcrypt.",
+    className: "md:col-span-1",
+    icon: <IconDesk className="h-8 w-8 text-neutral-500" />,
+  },
+  {
+    title: "Github Search App",
+    href: "https://github.com/MustafaM257/React-GithubSearchUsers",
+    description:
+      "React, Styled Components, REST API, GitHub API, Axios, Fusion Charts.",
+    className: "md:col-span-1",
+    icon: <IconUserSearch className="h-8 w-8 text-neutral-500" />,
   },
   {
     title: "Learning Management System",
     href: "https://github.com/MustafaM257/LMS",
     description:
       "PHP, JavaScript, MySQL, SMTP, REST API, HTML, CSS, Bootstrap.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-  },
-
-  {
-    title: "Github Search App",
-    href: "https://github.com/MustafaM257/React-GithubSearchUsers",
-    description:
-      "React, Styled Components, REST API, GitHub API, Axios, Fusion Charts.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "Job Portal APIS",
-    href: "https://github.com/MustafaM257/Jobs-API",
-
-    description: "Node.js, Express, MongoDB, Mongoose, REST API, JWT, Bcrypt.",
-    header: <Skeleton />,
     className: "md:col-span-2",
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    icon: <IconBooks className="h-8 w-8 text-neutral-500" />,
   },
+
   {
     title: "Modern Portfolio",
     href: "https://github.com/MustafaM257/Modern-Portfolio-NextJS",
     description: "React, TypeScript, Tailwind CSS, Next.js, Framer Motion.",
-    header: <Skeleton />,
     className: "md:col-span-2",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    icon: <IconUserScreen className="h-8 w-8 text-neutral-500" />,
   },
   {
     title: "URL Shortener",
     href: "https://github.com/MustafaM257/url-shortener",
     description:
       "Vue, Nuxt, TypeScript, Tailwind CSS, Server actions, Supabase, Prisma.",
-    header: <Skeleton />,
     className: "md:col-span-1",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    icon: <IconUnlink className="h-8 w-8 text-neutral-500" />,
   },
 ];
 
--- app/components/ui/projects-bento-grid.tsx
+++ app/components/ui/projects-bento-grid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
-
+import DotBackground from "./dot-background";
+import { IconBrandGithub } from "@tabler/icons-react";
 export const BentoGrid = ({
   className,
   children,
@@ -10,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid md:auto-rows-[20px] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
         className
       )}
     >
@@ -24,29 +25,39 @@ export const BentoGridItem = ({
   title,
   description,
   href,
-  header,
+  //   header,
   icon,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
+  //   header?: React.ReactNode;
   icon?: React.ReactNode;
   href?: string;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  bg-black/50 border-white/[0.2]  border  justify-between flex flex-col space-y-4",
+        "row-span-1   rounded-2xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  bg-black/50 border-white/[0.2]  border hover:border-white justify-between flex flex-col space-y-2",
         className
       )}
     >
-      {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-white mb-2 mt-2">{title}</div>
-        <div className="font-sans font-normal text-white text-xs ">
-          {description}
+      <div className="font-sans text-xl font-bold text-white mb-2 mt-2 flex flex=row items-center justify-between">
+        <h2>{title}</h2>
+        {icon}
+      </div>
+      <button className=" max-w-40 bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+        <span className="absolute inset-0 overflow-hidden rounded-full">
+          <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+        </span>
+        <div className="relative flex justify-between items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+          <span>{`Source Code`}</span>
+          <IconBrandGithub className="w-4 h-4" />
         </div>
+        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+      </button>
+      <div className="group-hover/bento:translate-x-2 transition duration-200">
+        <div className=" font-normal text-white text-base ">{description}</div>
       </div>
     </div>
   );

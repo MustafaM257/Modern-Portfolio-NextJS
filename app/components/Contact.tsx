c24b9b7dcef0902e8cb107211cd5ced5495f0e49
Mustafa Muhammad <mustafa.m257@proton.me>
2024-04-07 22:37:56 -0400

    Settled on contact card design, next to finalize and add data

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,69 +1,44 @@
+import React from "react";
+import { Meteors } from "./ui/metors";
 import Container from "./shared/Container";
-import Links from "./shared/Links";
-import { TypewriterEffect } from "./ui/typewriter-effect";
-import {
-  GlowingStarsBackgroundCard,
-  GlowingStarsDescription,
-  GlowingStarsTitle,
-} from "./ui/glowing-stars";
-
-const words = [
-  {
-    text: "Let's",
-  },
-  {
-    text: "transform",
-  },
-  {
-    text: "ideas",
-  },
-  {
-    text: "into",
-  },
-  {
-    text: "Reality!",
-    className: "text-blue-500 dark:text-blue-500",
-  },
-];
-const Icon = () => {
+export function Contact() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="h-4 w-4 text-white stroke-2"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-      />
-    </svg>
-  );
-};
-
-const Contact = () => {
-  return (
-    <Container id="contact" title="Contact" className="space-y-10 w-full ">
-      <TypewriterEffect words={words} />
-      <Links />
-      <div className="flex py-20 items-center justify-center antialiased">
-        <GlowingStarsBackgroundCard>
-          <GlowingStarsTitle>Next.js 14</GlowingStarsTitle>
-          <div className="flex justify-between items-end">
-            <GlowingStarsDescription>
-              The power of full-stack to the frontend. Read the release notes.
-            </GlowingStarsDescription>
-            <div className="h-8 w-8 rounded-full bg-[hsla(0,0%,100%,.1)] flex items-center justify-center">
-              <Icon />
+    <Container id="contact" title="Contact Me">
+      <div className="">
+        <div className=" w-full relative max-w-xs">
+          <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-white  transform scale-[0.80] bg-white rounded-full blur-3xl" />
+          <div className="relative shadow-xl bg-black border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+            <div className="h-5 w-5 rounded-full border flex items-center justify-center  mb-4 border-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-2 w-2 text-gray-300"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+                />
+              </svg>
             </div>
+            <h1 className="font-bold text-xl text-white mb-4 relative z-50">
+              Meteors because they&apos;re cool
+            </h1>
+            <p className="font-normal text-base text-white mb-4 relative z-50">
+              I don&apos;t know what to write so I&apos;ll just paste something
+              cool here. One more sentence because lorem ipsum is just
+              unacceptable. Won&apos;t ChatGPT the shit out of this.
+            </p>
+            Btn goes here if needed
+            {/* Meaty part - Meteor effect */}
+            <Meteors number={20} />
           </div>
-        </GlowingStarsBackgroundCard>
+        </div>
       </div>
     </Container>
   );
-};
-
+}
 export default Contact;
